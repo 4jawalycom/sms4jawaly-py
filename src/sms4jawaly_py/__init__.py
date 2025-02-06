fdda472ab52845b1d02c86957ab8b3293f2bf785
sameh adel <samehsoliman@samehs-MacBook-Pro.local>
2025-02-06 15:40:23 +0200

    Fix package exports

--- src/sms4jawaly_py/__init__.py
+++ src/sms4jawaly_py/__init__.py
@@ -6,22 +6,21 @@ A Python library for sending SMS messages through the 4jawaly SMS Gateway.
 
 Basic usage:
 
-    >>> from fourjawaly_sms import SMSGateway
-    >>> client = SMSGateway('your_api_key', 'your_api_secret', 'YOUR_SENDER_NAME')
+    >>> from fourjawaly_sms import SMS4JawalyClient
+    >>> client = SMS4JawalyClient('your_api_key', 'your_api_secret', 'YOUR_SENDER_NAME')
     >>> response = client.send_single_sms('966500000000', 'Hello from 4jawaly!')
     >>> print(response.success)
     True
 """
 
-from .sms_gateway import SMSGateway, SMSGatewayError
-from .models import SMSRequest, SMSResponse, Package, BalanceResponse
+from .sms_gateway import SMSGateway as SMS4JawalyClient
+from .models import SMSResponse, BalanceResponse, SenderNamesResponse
 
-__version__ = '1.0.0'
+__version__ = "1.0.0"
+
 __all__ = [
-    'SMSGateway',
-    'SMSGatewayError',
-    'SMSRequest',
-    'SMSResponse',
-    'Package',
-    'BalanceResponse'
+    "SMS4JawalyClient",
+    "SMSResponse",
+    "BalanceResponse",
+    "SenderNamesResponse"
 ]
